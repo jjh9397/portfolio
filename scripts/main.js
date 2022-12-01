fb49538c3a399ac8f1cb33aafb9930deeff9e605
jjh9397 <jasonhuang2023@u.northwestern.edu>
2022-12-01 01:35:03 -0600

    fix home pull-down alignment, fix pull-down event handlers

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,27 @@
 let home = document.getElementById("home");
+let home_pulldown = document.getElementById("home-pulldown");
 let contact = document.getElementById("contact");
+let contact_pulldown = document.getElementById("contact-pulldown");
 
-home.addEventListener("focus", focusHome);
-home.addEventListener("mouseover", focusHome);
-home.addEventListener("focusout", defocusHome);
-home.addEventListener("mouseout", defocusHome);
+home.addEventListener("focusin", focusHome);
+home.addEventListener("mouseenter", focusHome);
+home.addEventListener("focusleave", defocusHome);
+home.addEventListener("mouseleave", defocusHome);
 
-contact.addEventListener("focus", focusContact);
-contact.addEventListener("mouseover", focusContact);
-contact.addEventListener("focusout", defocusContact);
-contact.addEventListener("mouseout", defocusContact);
+home_pulldown.addEventListener("mouseenter", focusHome);
+home_pulldown.addEventListener("mouseleave", defocusHome);
+home_pulldown.addEventListener("focusin", focusHome);
+home_pulldown.addEventListener("focusleave", defocusHome);
+
+contact.addEventListener("focusin", focusContact);
+contact.addEventListener("mouseenter", focusContact);
+contact.addEventListener("focusleave", defocusContact);
+contact.addEventListener("mouseleave", defocusContact);
+
+contact_pulldown.addEventListener("mouseenter", focusContact);
+contact_pulldown.addEventListener("mouseleave", defocusContact);
+contact_pulldown.addEventListener("focusin", focusContact);
+contact_pulldown.addEventListener("focusleave", defocusContact);
 
 function focusHome(event)
 {
